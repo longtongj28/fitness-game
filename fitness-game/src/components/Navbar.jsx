b6b9d5ba15ad6020ae95f3d89e97ad0eb9ba2f24
Mark A. Carbajal <70996519+MarkCarbajal@users.noreply.github.com>
2022-02-23 20:33:02 -0800

    feat(navbar)

---
+++ fitness-game/src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './css/navbar.css';
+
+function Navbar() {
+  return ( 
+    <div className="Navbar">
+        <div className="leftSide">
+            <div className = "links">
+                <a href="/discover">Discover</a>
+                <a href="/fitstatus">Your FitZone</a>
+                <a href="/leaderboard">Leaderboard</a>
+                <a href="/rewards">Rewards</a>
+            </div>
+        </div>
+        <div className="rightSide"></div>
+
+        <div className="button">
+            <button className="btn">Sign Up</button>
+        </div>
+     </div>
+    
+  );
+}
+
+export default Navbar
--- fitness-game/src/components/pages/MapPage.js
+++ fitness-game/src/components/pages/MapPage.js
@@ -1,12 +1,14 @@
 import * as React from 'react'
 import ObjectiveMap from '../ObjectiveMap'
 import Sidebar from '../Sidebar'
+import Navbar from '../Navbar'
 
 
 const MapPage = () => {
     return (
         <>
         <div className="page">
+            <Navbar/>
             <ObjectiveMap/>
             <Sidebar/>
         </div>
